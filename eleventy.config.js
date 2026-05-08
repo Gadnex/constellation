01704454ec6dffb2686d0c8127eaa88e407ba70c
William Gadney <gadnex@gmail.com>
2026-05-08 18:43:02 +0200

    Change to 11ty

---
+++ eleventy.config.js
@@ -0,0 +1,37 @@
+import Shiki from "@shikijs/markdown-it";
+import MarkdownIt from "markdown-it";
+
+export default async function (eleventyConfig) {
+
+    eleventyConfig.setInputDirectory("src");
+
+    eleventyConfig.setTemplateFormats([
+        "html",
+        "md",
+        "jpg",
+        "png",
+        "svg",
+        "ico"
+    ]);
+
+    eleventyConfig.addPassthroughCopy("src/*/*.css");
+    eleventyConfig.addPassthroughCopy("src/*/*.js");
+
+    const options = {
+        html: true,
+        breaks: true,
+        linkify: true,
+    };
+
+    let markdownLib = MarkdownIt(options)
+        .use(
+            await Shiki({
+                themes: {
+                    light: 'github-dark',
+                    dark: 'github-dark',
+                }
+            }),
+        );
+
+    eleventyConfig.setLibrary("md", markdownLib);
+};
--- src/main/java/io/github/gadnex/constellation/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package io.github.gadnex.constellation;
-
-import org.jspecify.annotations.NullMarked;
